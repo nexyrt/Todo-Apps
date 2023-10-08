36a87f73951ceef947f61ae89750d9a4c3119ba1
Rizky Hamdani <rizkyhamdani67@gmail.com>
2023-10-09 01:02:56 +0800

    Done with the Add ToDo Modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,41 @@
+const todos = [];
+const RENDER_EVENT = 'render-todo';
+
+// Event Listener saat tombol Submit pada form ditekan
 document.addEventListener('DOMContentLoaded', function () {
-    const submitForm = document.getElementById('form');
-    submitForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-      addTodo();
-    });
+  const submitForm = document.getElementById('form');
+  submitForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addTodo();
   });
+});
+
+document.addEventListener(RENDER_EVENT, function () {
+  console.log(todos);
+});
+
+// Event Handler yang akan dijalankan saat tombol Submit pada form ditekan
+function addTodo() {
+  const textTodo = document.getElementById('inputToDo').value;
+  const timestamp = document.getElementById('inputDate').value;
+
+  const generatedID = generateId();
+  const todoObject = generateTodoObject(generatedID, textTodo, timestamp, false);
+  todos.push(todoObject);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+// Fungsi Helper untuk digunakan dalam Event Handler
+function generateId() {
+  return +new Date();
+}
+ 
+function generateTodoObject(id, task, timestamp, isCompleted) {
+  return {
+    id,
+    task,
+    timestamp,
+    isCompleted
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}",
     "./node_modules/flowbite/**/*.js"],
   theme: {
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif']
+    },
     extend: {},
   },
 }
